dfe70cb9a4eecb4f39072babce1095550e69626c
Dwij Joshi <dwijjoshi14@gmail.com>
2026-05-08 21:15:42 -0400

    Add Circuit class and related structures

--- src/circuit.h
+++ src/circuit.h
@@ -1,1 +1,38 @@
+#pragma once
+#include "resistor.h"
+#include <vector>
+#include <string>
 
+enum class CircuitType { SERIES, PARALLEL };
+
+struct ComponentResult {
+    std::string name;
+    double resistance_ohms;
+    double voltage_drop_V;
+    double current_A;
+    double power_W;
+};
+
+struct CircuitResult {
+    CircuitType type;
+    double supply_voltage_V;
+    double total_resistance_ohms;
+    double total_current_A;
+    double total_power_W;
+    std::vector<ComponentResult> components;
+};
+
+class Circuit {
+public:
+    Circuit(double supply_voltage, CircuitType type);
+    void addResistor(const std::string& name, double ohms);
+    CircuitResult analyze() const;
+
+private:
+    double supply_voltage_;
+    CircuitType type_;
+    std::vector<Resistor> resistors_;
+
+    double calcSeriesTotal() const;
+    double calcParallelTotal() const;
+};
